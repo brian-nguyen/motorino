748d4926fff2a7779501633d4185684a45665993
SeeratSek <seeratsek@gmail.com>
2018-03-24 01:44:47 -0700

    change productvalidator rendering bc brian didn't like my way

--- src/components/ProductValidator.js
+++ src/components/ProductValidator.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 export default class ProductValidator extends React.Component {
   render() {
+    if (!this.props.success) {
+        return (
+        <div className="alert alert-danger" role="alert">{this.props.failureType}</div>
+        );
+    }
+
     return (
-      <div style={s.listContainer}>
-        {this.props.success && <div className="alert alert-success" role="alert">Success!</div>}
-        {!this.props.success && <div className="alert alert-danger" role="alert">{this.props.failureType}</div>}
-      </div>
+        <div className="alert alert-success" role="alert">Success!</div>
     );
   }
 }
