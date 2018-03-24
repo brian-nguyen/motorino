5f9b99109a8f87da8c19e358a478bb8f9d3272ac
Brian Nguyen <briannguyen992@gmail.com>
2018-03-23 21:53:02 -0700

    Adding to state

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
 export default class ProductForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      email: '',
+      price: '',
+      competitor: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <form className="m-3">
+      <form className="m-3" onSubmit={this.handleSubmit}>
         <div className="form-group">
           Product Name
-          <input id="email" type="email" className="form-control" placeholder="Enter email" />
+          <input value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })} id="email" type="email" className="form-control" placeholder="Enter email" />
         </div>
         <div className="form-group">
           Price
-          <input id="email" type="email" className="form-control" placeholder="Enter email" />
+          <input value={this.state.price} onChange={(e) => this.setState({ price: e.target.value })} id="price" type="email" className="form-control" placeholder="Enter email" />
         </div>
         <div className="form-group">
           Competitor
-          <select className="form-control">
+          <select value={this.state.competitor} onChange={(e) => this.setState({ competitor: e.target.value })} className="form-control">
             <option>Amazon</option>
             <option>Toys R Us</option>
             <option>Walmart</option>
