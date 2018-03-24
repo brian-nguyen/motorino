86036185161ea36ecf17d0cd55a1b22b7188c06d
Brian Nguyen <briannguyen992@gmail.com>
2018-03-23 20:49:36 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="alert alert-primary" role="alert">
+          This is a primary alert—check it out!
+        </div>
       </div>
     );
   }
