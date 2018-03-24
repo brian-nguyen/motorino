1e29d49fc64800bb4d4b7c8257aaa050a24f4ded
Joey Li <joeyli96@hotmail.com>
2018-03-24 01:13:19 -0700

    Able to retrieve image text from Google Vision.

--- src/Ocr.js
+++ src/Ocr.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { API_KEY } from './API_KEY.js';
 
 class Ocr extends Component {
   constructor(props) {
@@ -9,16 +11,27 @@ class Ocr extends Component {
   }
 
   handleChangeImage = async (evt) => {
-    console.log("Uploading");
     var reader = new FileReader();
     var file = evt.target.files[0];
-    var self = this;
-    reader.onload = (upload) => {
-        this.setState({
-            image: upload.target.result
-        }, () => console.log(this.state.image, "uploaded"));
-    };
     reader.readAsDataURL(file);
+    reader.onload = async function() {
+      const content = reader.result.replace('data:image/png;base64,','');
+      const request = {
+        "requests": [
+          {
+            "image": {
+              content
+            },
+            "features": [
+              {
+                "type":"TEXT_DETECTION"
+              }
+            ]
+          }
+        ]
+      }
+      let response = await axios.post('https://vision.googleapis.com/v1/images:annotate?key=' + API_KEY, request);
+    }
   }
 
   render() {
